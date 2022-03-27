355ae836f403b5dfdef044beeb5177f5c97143c2
Yashar Js <Yashar@Icys-MacBook-Pro.local>
2022-03-27 01:49:13 -0400

    set up fullstack structure

--- server/server.js
+++ server/server.js
@@ -9,6 +9,8 @@ const {authMiddleware} = require('./utils/auth')
 const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
 
+const path = require('path')
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -37,6 +39,15 @@ startServer();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once("open", () => {
   app.listen(PORT, () => {
